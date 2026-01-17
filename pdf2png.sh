11e71f62f7e3a83f4e7fd847b7cc4b87a78b3145
Benjamin Steenkamer <13274734+bsteen@users.noreply.github.com>
2026-01-16 20:52:18 -0500

    Check if file is provided

--- pdf2png.sh
+++ pdf2png.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# (C) 2023, 2025 Benjamin Steenkamer
+# (C) 2023, 2025 - 2026 Benjamin Steenkamer
 # Convert `.pdf` to a set of `.png`s
 # Try to maintain good quality in the process (increase the density if results are not good enough)
+if [[ ! -f "$1" ]]; then
+    echo "File not found: \"$1\""
+    exit 1
+fi
+
 convert -alpha remove -density 300 "$1" "${1%.pdf}.png"
--- pdf2png2pdf.sh
+++ pdf2png2pdf.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# (C) 2020 - 2023, 2025 Benjamin Steenkamer
+# (C) 2020 - 2023, 2025 - 2026 Benjamin Steenkamer
 # Convert .pdf to .png and then back to .pdf.
 # Try to  maintain good quality in the process (increase the density if results are not good enough).
 # Useful for removing hyperlinks and searchable text in PDFs.
+if [[ ! -f "$1" ]]; then
+    echo "File not found: \"$1\""
+    exit 1
+fi
 
 TEMP_DIR=$(mktemp -d)
 
@@ -10,8 +14,9 @@ TEMP_DIR=$(mktemp -d)
 echo "Converting PDF to PNGs..."
 convert -density 300 "$1" ${TEMP_DIR}/page.png
 
-echo "Converting PNGs to PDF..."
 # Get PNG names by numeric order
+echo "Converting PNGs to PDF..."
 convert $(ls -1 ${TEMP_DIR}/*.png | sort -V) "${1%.pdf}-converted.pdf"
+
 rm -rf ${TEMP_DIR}
 echo "Done"
